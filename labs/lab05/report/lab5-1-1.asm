11679ef302a2608b5dfdb614ca47a0ae6acd2914
ivansalinder <145552486+ivansalinder@users.noreply.github.com>
2024-02-10 22:30:26 +0300

    Add files via upload

---
+++ labs/lab05/report/lab5-1-1.asm
@@ -0,0 +1,26 @@
+SECTION .data
+msg: DB 'Введите строку:',10
+msgLen: EQU $-msg
+SECTION .bss
+buf1: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax,4
+mov ebx,1
+mov ecx,msg
+mov edx,msgLen
+int 80h
+mov eax, 3
+mov ebx, 0
+mov ecx, buf1
+mov edx, 80
+int 80h
+mov eax,4
+mov ebx,1
+mov ecx,buf1
+mov edx,buf1
+int 80h
+mov eax,1
+mov ebx,0
+int 80h
